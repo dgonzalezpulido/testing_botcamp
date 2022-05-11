3c3db0b746e704d172488525a032ef6da178be66
Daniel Gonzalez <d.gonzalez.pulido@accenture.com>
2022-05-11 10:58:07 +0000

    test

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami         = "ami-0eb7496c2e0403237"
+  ami         = "ami-09439f09c55136ecf"
   instance_type = "t2.micro"
   tags = {
     Name = var.instance_name
