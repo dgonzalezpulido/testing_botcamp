8ee9c1c191644bb3219f9b1aad2b46548da8c61a
dgonzalezpulido <84788637+dgonzalezpulido@users.noreply.github.com>
2022-05-11 16:57:48 +0200

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "ec2" {
   ami         = "ami-0eb7496c2e0403237"
   instance_type = "t2.micro"
+  iam_instance_profile = "Ec2RoleForSSM"
+  
   tags = {
     Name = var.instance_name
+    Deployment = "bootcamp-target"
+    User = var.user_name
   }
 }
